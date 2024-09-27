76025d92d5f2effc124e4c25bf776ed1eef25730
Gregory Serrao <gregperes84@gmail.com>
2024-09-27 12:33:53 -0400

    feat: add tailwind support

--- public/dist/js/main.BYLOuhG-.js
+++
@@ -1,1 +0,0 @@
-const o="background-color: darkblue; color: white; font-style: italic; border: 5px solid hotpink; font-size: 2em;";console.log("%cHello Lalaz",o);
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,php,twig}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
