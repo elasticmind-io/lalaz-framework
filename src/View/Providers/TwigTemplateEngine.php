b384c6256480106a3c3c88a918d3129b9a0b7849
Gregory Serrao <gregperes84@gmail.com>
2025-03-25 19:27:21 -0400

    feat: add view extensions support

--- src/View/Providers/TwigTemplateEngine.php
+++ src/View/Providers/TwigTemplateEngine.php
@@ -21,6 +21,7 @@ class TwigTemplateEngine implements TemplateEngineInterface
 
         $this->twig = new Environment($loader);
         $this->attachUtilFunctions();
+        $this->attachExtensions();
     }
 
     public function render(string $template, array $data = []): string
@@ -40,6 +41,35 @@ class TwigTemplateEngine implements TemplateEngineInterface
     {
         foreach (Utils::all() as $util) {
             $this->twig->addFunction($util);
+        }
+    }
+
+    /**
+     * Loads and executes external Twig extension files defined in the VIEW_EXTENSIONS config key.
+     *
+     * Each file must return a callable that receives the Twig Environment instance,
+     * allowing the user to register functions, filters, or global variables.
+     *
+     * Example of a valid extension file:
+     *
+     * return function (\Twig\Environment $twig) {
+     *     $twig->addFunction(new \Twig\TwigFunction('money', fn($v) => 'R$ ' . number_format($v, 2, ',', '.')));
+     *     $twig->addGlobal('now', date('Y-m-d H:i:s'));
+     * };
+     *
+     * @return void
+     */
+    private function attachExtensions(): void
+    {
+        $extensionFiles = config('VIEW_EXTENSIONS', []);
+
+        foreach ($extensionFiles as $file) {
+            if (file_exists($file)) {
+                $callback = require $file;
+                if (is_callable($callback)) {
+                    $callback($this->twig);
+                }
+            }
         }
     }
 }
